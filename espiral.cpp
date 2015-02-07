fcdda41e14a1f4a70dcd57b53f9142b2092825ea
arquimago <thales@alvesnet.net>
2015-02-07 15:40:50 -0300

    Dojo Espiral em C

---
+++ espiral.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int tabela[100][100];
+
+void imprime(int linhas,int colunas){
+	int i=0,j=0;
+	for(i=0;i<linhas;i++){
+		for(j=0;j<colunas;j++){
+			printf("%4d ",tabela[i][j]);
+		}
+		printf("\n");
+	}
+	return;
+}
+
+void gera_espiral(int linhas,int colunas){
+	int i=0,j=0,k=0,l=0;
+	int N=0,E=1,S=0,W=0;
+	l=colunas*linhas;
+	for(k=0;k<l;k++){
+		tabela[i][j]=k+1;
+		if(E==1){
+			j++;
+			if(tabela[i][j+1]!=0) {
+				E=0;
+				S=1;	
+			}
+		}else if(S==1){
+			i++;
+			if(tabela[i+1][j]!=0){
+				S=0;
+				W=1;
+			}
+		}else if(W==1){
+			j--;
+			if(tabela[i][j-1]!=0){
+				W=0;
+				N=1;
+			}
+		}else if(N==1){
+			i--;
+			if(tabela[i-1][j]!=0){
+				N=0;
+				E=1;
+			}
+		}
+	}
+	return;
+}
+
+int main(){
+	int i=0,j=0,linhas=0,colunas=0;
+	scanf("%d %d",&linhas,&colunas);
+	memset(*tabela,0xff,sizeof(tabela));
+	for(i=0;i<linhas;i++){
+		for(j=0;j<colunas;j++){
+			tabela[i][j]=0;
+		}
+	}	
+	gera_espiral(linhas,colunas);
+	imprime(linhas,colunas);
+	return 0;
+}
